783b27bffa074aa0d608e28e0a9ff9724bd5f22b
panda <SmelayaPanda@mail.ru>
2018-09-15 22:46:45 +0700

    input and bnt

--- App.js
+++ App.js
@@ -1,24 +1,45 @@
 import React from 'react';
-import {StyleSheet, Text, TextInput, View} from 'react-native';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 
 export default class App extends React.Component {
     state = {
-        placeName: ''
+        placeName: '',
+        places: []
     }
 
     onPlaceNameChange = val => {
         this.setState({placeName: val});
     }
 
+    placeSubmit = () => {
+        if (!this.state.placeName.trim()) return
+        this.setState(prevState => ({
+            placeName: '',
+            places: prevState.places.concat(prevState.placeName.trim())
+        }));
+
+    }
+
     render() {
         return (
             <View style={styles.container}>
                 <Text>Hello Panda!</Text>
-                <TextInput
-                    style={{width: 300}}
-                    placeholder={'An awesome place'}
-                    value={this.state.placeName}
-                    onChangeText={this.onPlaceNameChange}/>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.placeInput}
+                        placeholder={'An awesome place'}
+                        value={this.state.placeName}
+                        onChangeText={this.onPlaceNameChange}/>
+                    <Button
+                        style={styles.placeBtn}
+                        title={'Add'}
+                        onPress={this.placeSubmit}/>
+                </View>
+                <View>
+                    {this.state.places.map((place, i) => {
+                        return <Text key={i}>{place}</Text>
+                    })}
+                </View>
             </View>
         );
     }
@@ -27,8 +48,21 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        padding: 25,
         backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
     },
+    inputContainer: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    placeInput: {
+        width: '70%'
+    },
+    placeBtn: {
+        width: '30%'
+    }
 });
